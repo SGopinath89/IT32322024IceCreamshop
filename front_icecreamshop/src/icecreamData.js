9919e4996f38909425458b8b4b59b613e69b1d1d
TharushiRanawaka <tharushinimnadi1999@gmail.com>
2024-06-09 12:54:57 +0530

    Updated the icecreamData.js

--- front_icecreamshop/src/icecreamData.js
+++ front_icecreamshop/src/icecreamData.js
@@ -16,106 +16,106 @@ const IcecreamList=[
             "Vanila",
             "Strawberry"
         ],
-        "description":"Sweet strawberry ice cream that has juicy strawberries mixed throughout.",
+        "description":"A delightful blend of sweet, juicy strawberries and creamy vanilla, offering a refreshing and smooth taste that combines the best of both fruity and classic flavors.",
         "image":"https://www.creationsbykara.com/wp-content/uploads/2021/03/Homemade-Strawberry-Ice-Cream-19-2.jpg"
     },
     {
         "name":"Vanilla Mint",
         "price":400,
         "category":"Vanila",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":" A refreshing fusion of creamy vanilla and cool mint, delivering a smooth, invigorating flavor that perfectly balances sweetness with a hint of crisp freshness.",
         "image":"https://cleanfoodcrush.com/wp-content/uploads/2021/09/clean-eating-simple-Homemade-Mint-Chocolate-Chip-Ice-Cream-recipe.jpg"
     },
     {
         "name":"Chocalate Blueberry",
         "price":200,
         "category":"Chocalote",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"A luscious combination of rich, decadent chocolate and sweet, tangy blueberries, creating a unique and indulgent flavor experience with a perfect balance of sweetness and tartness.",
         "image":"https://cookbookshelf.wordpress.com/wp-content/uploads/2014/11/done.jpg"
     },
     {
         "name":"Mint Strawberry",
         "price":240,
         "category":"Strawberry",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"A refreshing mix of cool mint and sweet, juicy strawberries, offering a unique and invigorating flavor combination that's both fresh and fruity.",
         "image":"https://media.istockphoto.com/photos/strawberry-cream-ice-cream-in-white-bowl-picture-id678771308?k=20&m=678771308&s=612x612&w=0&h=Agfy1tHh7wXOx9zPnTmWbHb8byhEyyzVqqIQnEyL9UY="
     },
     {
         "name":"Milk Choco",
         "price":360,
         "category":"Chocolate",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"Smooth and creamy milk chocolate ice cream, made with the finest cocoa for a rich and indulgent chocolate experience.",
         "image":"https://deliciousmadeeasy.com/wp-content/uploads/2015/12/Dairy-Free-Chocolate-Ice-Cream-735x640.jpg"
     },
     {
         "name":"Caremel",
         "price":400,
         "category":"None",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"Luxuriously smooth ice cream with a rich caramel flavor, providing a perfect balance of buttery sweetness and creamy texture.",
         "image":"https://www.paleorunningmomma.com/wp-content/uploads/2018/06/salted-caramel-ice-cream-9.jpg"
     },
     {
         "name":"Chocalate",
         "price":440,
         "category":"Chocolate",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"Decadent and rich, made with premium cocoa for a deep, indulgent flavor, velvety smooth and a favorite for all chocolate lovers.",
         "image":"https://hamiltonbeach.ca/media/recipes/chocolate-ice-cream.jpg"
     },
     {
         "name":"Coffee",
         "price":200,
         "category":"None",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"A robust and creamy ice cream with the bold flavor of freshly brewed coffee, perfect for coffee aficionados seeking a smooth, caffeinated treat.",
         "image":"https://celebratingsweets.com/wp-content/uploads/2021/06/Coffee-Ice-Cream-1-2.jpg"
     },
     {
         "name":"Bubble Gum",
         "price":300,
         "category":"None",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"Fun and colorful, this ice cream has the sweet and nostalgic taste of bubble gum, making it a delightful treat for all ages.",
         "image":"https://mjbakerfoodservice.co.uk/media/catalog/product/cache/e70602422d911f0edb0b0d50a9ac95bc/b/c/bc49a79bbae7d2638c1ff4ad93ae52fc.jpg"
     },
     {
         "name":"Strawberry Oreo",
         "price":400,
         "category":"Strawberry",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
+        "description":"A delightful blend of sweet, ripe strawberries and crunchy Oreo cookies, offering a perfect balance of fruity and chocolatey goodness.",
         "image":"https://celebratingsweets.com/wp-content/uploads/2021/06/Coffee-Ice-Cream-1-2.jpg"
     },
     {
         "name":"Oreo Cookies",
         "price":200,
         "category":"None",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
-        "image":"https://5.imimg.com/data5/HW/SD/MY-29540739/pepper-barbecue-chicken-1000x1000.png"
+        "description":"Creamy ice cream packed with chunks of Oreo cookies, combining the classic flavor of vanilla with the crunch of chocolate cookies.",
+        "image":"https://rosebakes.com/wp-content/uploads/2018/08/Fresh-Scooped-OREO-Cookies-Cream-Ice-Cream-Tight.jpg"
     },
     {
         "name":"Red Velvet",
         "price":240,
         "category":"None",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
-        "image":"https://celebratingsweets.com/wp-content/uploads/2021/06/Coffee-Ice-Cream-1-2.jpg"
+        "description":"Rich and creamy ice cream with the distinctive flavor of red velvet cake, including hints of cocoa and cream cheese frosting.",
+        "image":"https://www.urbanbakes.com/wp-content/uploads/2019/05/Red-Velvet-Ice-Cream-urbanbakes.com-6.1.jpg"
     },
     {
         "name":"Almond",
         "price":360,
         "category":"None",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
-        "image":"https://celebratingsweets.com/wp-content/uploads/2021/06/Coffee-Ice-Cream-1-2.jpg"
+        "description":"Creamy and nutty, made with real almonds for a rich, smooth ice cream that offers a delightful crunch and a toasty almond flavor.",
+        "image":"https://sugarfreelondoner.com/wp-content/uploads/2022/07/Almond-Milk-Ice-Cream-1200.jpg"
     },
     {
         "name":"Vanila Crispy",
         "price":400,
         "category":"Vanila",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
-        "image":"https://celebratingsweets.com/wp-content/uploads/2021/06/Coffee-Ice-Cream-1-2.jpg"
+        "description":"Classic vanilla ice cream with a crispy twist, featuring crunchy bits for an added texture that perfectly complements the smooth vanilla base.",
+        "image":"https://cdn11.bigcommerce.com/s-cjh14ahqln/product_images/uploaded_images/crispy-biscotti-ice-cream-topping.jpg"
     },
     {
         "name":"Coffee Vanila",
         "price":440,
         "category":"Vanila",
-        "description":"A sweet ice cream that is flavored with a wonderful blend of vanilla,",
-        "image":"https://celebratingsweets.com/wp-content/uploads/2021/06/Coffee-Ice-Cream-1-2.jpg"
+        "description":"A harmonious blend of bold coffee and smooth vanilla, creating a creamy and balanced flavor that's both invigorating and soothing.",
+        "image":"https://celebratingsweets.com/wp-content/uploads/2021/06/Coffee-Ice-Cream-4.webp"
     },
 ]
 
